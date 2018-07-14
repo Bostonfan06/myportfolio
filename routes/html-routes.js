593d63c9237df9de1fbedef1916aee89bc347835
Sean McPheeters <alexissean@Seans-MacBook-Pro.local>
2018-07-13 17:37:55 -0700

    Updated All Files

---
+++ routes/html-routes.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+
+//Routes to all HTML
+module.exports = function (app) {
+
+app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname + '/public/opening.html'));
+        });
+
+app.get('/home', function(req, res){
+res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/projects', function(req, res){
+res.sendFile(path.join(__dirname + '/public/projects.html'));
+});
+
+app.get('/about', function(req, res){
+res.sendFile(path.join(__dirname + '/public/about.html'));
+});
+
+app.get('/contact', function(req, res){
+res.sendFile(path.join(__dirname + '/public/contact.html'));
+});
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+
+var PORT = process.env.PORT || 8080
+
+//Routes to all HTML Pages//
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/opening.html'));
+});
+
+app.get('/home', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/projects', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/projects.html'));
+});
+
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/about.html'));
+});
+
+app.get('/contact', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/contact.html'));
+});
+
+require('./routes/html-routes.js')(app)
+
+app.listen(PORT, function(){
+  console.log("App listening on PORT: " + PORT)
+})
