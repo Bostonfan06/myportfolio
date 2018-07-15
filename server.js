39924830702bd7c32f54ec715d3f1f3a63cd3bf0
Sean McPheeters <McPheetersSean@Seans-iMac.local>
2018-07-15 13:49:24 -0700

    Updated Files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/projects', function(req, res){
