af5621c537c45bf6e90ad636026015fb35e5b881
Sean McPheeters <McPheetersSean@Seans-iMac.local>
2018-07-15 13:55:54 -0700

    Updated Files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ var path = require('path');
 module.exports = function (app) {
 
 app.get('/', function(req, res){
-res.sendFile(path.join(__dirname + '/public/index.html'));
+res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/projects', function(req, res){
