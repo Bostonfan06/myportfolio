8f73936f574153683837c0c1bf13421797e46df6
Sean McPheeters <mcpheeterssean@gmail.com>
2018-07-20 20:21:30 -0700

    Updated Files

--- server.js
+++ server.js
@@ -8,20 +8,24 @@ var PORT = process.env.PORT || 8080
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/projects', function(req, res){
-  res.sendFile(path.join(__dirname + '/projects.html'));
+  res.sendFile(path.join(__dirname + './projects.html'));
 });
 
 app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname + '/about.html'));
+  res.sendFile(path.join(__dirname + './about.html'));
 });
 
 app.get('/contact', function(req, res){
-  res.sendFile(path.join(__dirname + '/contact.html'));
+  res.sendFile(path.join(__dirname + './contact.html'));
 });
 
 require('./routes/html-routes.js')(app)
