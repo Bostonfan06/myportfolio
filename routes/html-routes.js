6d0d88a165f9744de3bc0aaa075a43ee0ab7016d
Sean McPheeters <mcpheeterssean@gmail.com>
2018-07-20 20:38:40 -0700

    Updated HTML

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,18 +6,18 @@ var path = require('path');
 module.exports = function (app) {
 
 app.get('/', function(req, res){
-res.sendFile(path.join(__dirname + '/index.html'));
+res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
 app.get('/projects', function(req, res){
-res.sendFile(path.join(__dirname + '/public/projects.html'));
+res.sendFile(path.join(__dirname + 'public/projects.html'));
 });
 
 app.get('/about', function(req, res){
-res.sendFile(path.join(__dirname + '/public/about.html'));
+res.sendFile(path.join(__dirname + 'public/about.html'));
 });
 
 app.get('/contact', function(req, res){
-res.sendFile(path.join(__dirname + '/public/contact.html'));
+res.sendFile(path.join(__dirname + 'public/contact.html'));
 });
 }
